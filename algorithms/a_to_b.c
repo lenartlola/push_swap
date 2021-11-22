6776c1a86d6c3143938b2a24d5d9e0048439c2e2
0x1nf3ct3d <leetpwnsroot@gmail.com>
2021-11-22 11:27:21 +0100

    Add comments

--- algorithms/a_to_b.c
+++ algorithms/a_to_b.c
@@ -28,6 +28,16 @@ static int	exceptions(int r, t_stack *a, t_stack *b)
 		return (1);
 }
 
+/*
+ * If the top of the stack a is bigger than the big_pivot,
+ * then rotate the stack a and add 1 to var->ra.
+ * Othwewise, push the top of the stack a to the stack b,
+ * and add 1 to var->pb.
+ * If the top of the stack b is bigger than the small_pivot,
+ * then rotate the stack b, we also have to add 1 to rb, keeping track
+ * of the operations been made.
+ */
+
 static void	push_rotate_a(t_stack *a, t_stack *b, t_value *var)
 {
 	if (a->top->value > var->big_pivot)
@@ -89,6 +99,20 @@ static void	back_to_orig_rb(t_stack *a, t_stack *b, int *count, t_value *var)
 			reverse_rotate_stack(b, B);
 	}
 }
+
+/*
+ * Select two pivots,
+ * big_pivot = (min + max) / 2
+ * small_pivot = (min + big_pivot) / 2
+ * While there is size of stack a, do the push_rotate_a.
+ * Call the function recursively.
+ * a_to_b(number of ra times)(1)
+ * b_to_a(number of rb times)(2)
+ * b_to_a(number of pb - rb times)(3)
+ * After counting ra and rb, we check if ra is bigger than
+ * rb, if so, it means that ra left and so we have to repeat the process
+ * as long as it left values.
+ */
 
 void	a_to_b(int r, t_stack *a, t_stack *b, int *count)
 {
